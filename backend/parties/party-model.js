2bf0eabf407e91faf2008b0c500bd9ea69b29b61
Brennon Nuckols <brennuck@gmail.com>
2020-06-29 15:08:44 -0600

    GET /:id working

--- backend/parties/party-model.js
+++ backend/parties/party-model.js
@@ -1,9 +1,14 @@
 const db = require('../data/dbConfig.js');
 
 module.exports = {
-    find
+    find,
+    findById,
 }
 
 function find() {
     return db("parties");
+}
+
+function findById(id) {
+    return db("parties").where({ id })
 }
--- backend/parties/party-router.js
+++ backend/parties/party-router.js
@@ -14,4 +14,20 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  Parties.findById(id)
+    .then(party => {
+      if (party) {
+        res.json(party)
+      } else {
+        res.status(404).json({ message: "Could not find party with given id" })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to get party" })
+    })
+})
+
 module.exports = router;
